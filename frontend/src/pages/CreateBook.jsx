db9df49101f12da330e9573240d0b97194288981
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2024-01-31 16:48:36 +0100

    snackbar

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -3,6 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import  axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 
 function CreateBook() {
@@ -11,6 +12,7 @@ function CreateBook() {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleSaveBook = () => {
     const data = {
@@ -23,11 +25,13 @@ function CreateBook() {
       .post('http://localhost:5555/books', data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Created successfully', { variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Chack console');
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' })
         console.log(error);
       });
   };
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import  axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useSnackbar } from 'notistack';
 
 
 function EditBook() {
@@ -13,6 +13,7 @@ function EditBook() {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const {id} = useParams();
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     setLoading(true);
@@ -42,11 +43,13 @@ function EditBook() {
       .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Edited successfully', { variant: 'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please Chack console');
+        //alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' })
         console.log(error);
       });
   };
